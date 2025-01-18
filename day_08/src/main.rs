5500207981cf7bda41762f3c9d9081781195cbbe
Emre Erkmen <erkmene@gmail.com>
2025-01-18 11:07:54 +0300

    Day 8

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -27,7 +27,7 @@ fn parse_input_file(file: &str) -> NodeMap {
     for row in 0..height {
         for col in 0..width {
             let cell = &map[row as usize][col as usize].chars().next().unwrap();
-            // If not .
+            // If not "."
             if *cell as u32 != 46 {
                 let node_collection = nodes.entry(cell.to_string()).or_insert(Vec::new());
                 node_collection.push((row as i32, col as i32))
@@ -46,27 +46,36 @@ fn is_inside_map(pos: (i32, i32), width: i32, height: i32) -> bool {
     pos.0 >= 0 && pos.1 >= 0 && pos.0 < height as i32 && pos.1 < width as i32
 }
 
-fn compute_anti_nodes(node_map: &NodeMap) -> HashSet<(i32, i32)> {
+fn compute_anti_nodes(node_map: &NodeMap, resonant: bool) -> HashSet<(i32, i32)> {
     let nodes = &node_map.nodes;
     let mut anti_nodes: HashSet<(i32, i32)> = HashSet::new();
-    // let mut anti_nodes_map: HashMap<String, Node> = HashMap::new();
     for id in nodes.keys() {
         let nodes_for_id = nodes.get(id).unwrap();
         for node in nodes_for_id {
-            // println!(">>> {:?}", node);
             for other_node in nodes_for_id {
-                // println!("> {:?}", other_node);
                 if node != other_node {
-                    let distance = (other_node.0 - node.0, other_node.1 - node.1);
-                    let anti_node_pos = (node.0 - distance.0, node.1 - distance.1);
-                    if is_inside_map(anti_node_pos, node_map.width, node_map.height) {
-                        anti_nodes.insert(anti_node_pos);
-                    }
-                    let anti_node_pos = (other_node.0 + distance.0, other_node.1 + distance.1);
-                    if is_inside_map(anti_node_pos, node_map.width, node_map.height) {
-                        anti_nodes.insert(anti_node_pos);
+                    for direction in [-1, 1] {
+                        let distance = (other_node.0 - node.0, other_node.1 - node.1);
+                        let mut anti_node = if direction == -1 { *node } else { *other_node };
+                        loop {
+                            anti_node = (
+                                anti_node.0 + (distance.0 * direction),
+                                anti_node.1 + (distance.1 * direction),
+                            );
+                            if is_inside_map(anti_node, node_map.width, node_map.height) {
+                                anti_nodes.insert(anti_node);
+                            } else {
+                                break;
+                            }
+                            if !resonant {
+                                break;
+                            }
+                        }
                     }
                 }
+            }
+            if resonant {
+                anti_nodes.insert(*node);
             }
         }
     }
@@ -75,11 +84,14 @@ fn compute_anti_nodes(node_map: &NodeMap) -> HashSet<(i32, i32)> {
 
 fn main() {
     let test_data = parse_input_file("08.test.dat");
-    let anti_nodes = compute_anti_nodes(&test_data);
-    // println!("{:?}", anti_nodes);
+    let anti_nodes = compute_anti_nodes(&test_data, false);
     assert!(anti_nodes.len() == 14);
+    let resonant_anti_nodes = compute_anti_nodes(&test_data, true);
+    assert!(resonant_anti_nodes.len() == 34);
+
     let data = parse_input_file("08.dat");
-    let anti_nodes = compute_anti_nodes(&data);
-    // println!("{:?}", anti_nodes);
+    let anti_nodes = compute_anti_nodes(&data, false);
     println!("First answer: {}", anti_nodes.len());
+    let resonant_anti_nodes = compute_anti_nodes(&data, true);
+    println!("Second answer: {}", resonant_anti_nodes.len());
 }
